a086902295da02b702f0204d20a128a0c857c4af
Ftnt <63697694+Ftnt@users.noreply.github.com>
2022-05-03 22:50:05 -0500

    configure swagger in the app.ts file

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -2,6 +2,11 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import clientRoutes from '../routes/client.routes';
+import {option} from './swaggerOptions';
+
+//swagger
+import swaggerIU from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
 
 const app = express();
 
@@ -9,6 +14,9 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 
+const specs = swaggerJsDoc(option)
+
 app.use(clientRoutes)
+app.use('/docs', swaggerIU.serve, swaggerIU.setup(specs))
 
 export default app
